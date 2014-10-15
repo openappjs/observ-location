5f4dbd874d73c33b69cdb963d610489a4a437808
Michael Williams <dinosaur@riseup.net>
2014-10-15 14:02:11 -0700

    add change tests

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 var test = require('tape');
 var window = require('global/window');
 var getIn = require('get-in');
+var Url = require('url');
 
 var ObservLocation;
 
@@ -20,6 +21,49 @@ test("get location", function (t) {
   function end () {
     location.stop();
     unlisten();
+    t.end();
+  }
+});
+
+test("change location path", function (t) {
+  var location = ObservLocation({
+    pushState: true,
+  });
+  var origHref = window.location.href;
+  var path = "/asdf";
+  var unlisten = location(function (value) {
+    t.equal(value, getIn(window, ['location', 'href']));
+    t.equal(Url.parse(value).path, path);
+    end();
+  });
+  location.change(path);
+  function end () {
+    unlisten();
+    // reset location to original
+    location.set(origHref);
+    location.stop();
+    t.end();
+  }
+});
+
+test("change location hash", function (t) {
+  var location = ObservLocation({
+    pushState: false,
+    hash: true,
+  });
+  var origHref = window.location.href;
+  var hash = "#asdf";
+  var unlisten = location(function (value) {
+    t.equal(value, getIn(window, ['location', 'href']));
+    t.equal(Url.parse(value).hash, hash);
+    end();
+  });
+  location.change(hash);
+  function end () {
+    unlisten();
+    // reset location to original
+    location.set(origHref);
+    location.stop();
     t.end();
   }
 });
